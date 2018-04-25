47603c0e6d0eedc276a55be1177e668d3905765f
Saige Leslie <saigeleslie@gmail.com>
2018-04-25 04:20:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,10 +8,10 @@ class Dog
   end
   
   def breed=(dog_breed)
-    @this_dogs_name = dog_breed
+    @this_dogs_breed = dog_breed
   end
   
   def breed
-    @this_dogs_name
+    @this_dogs_breed
   end
 end
